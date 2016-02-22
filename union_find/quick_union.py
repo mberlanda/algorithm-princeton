75c8dc86a18efab1b78897eef0033fa5fd6fcf79
Mauro Berlanda <m.berlanda@fit2you.it>
2016-02-22 10:59:47 +0100

    quick union py created and tested

--- union_find/quick_union.py
+++ union_find/quick_union.py
@@ -1,1 +1,38 @@
+import unittest
 
+class QuickUnionUF:
+
+    def __init__(self, n):
+      self.n = n
+      self.array = list(range(n))
+
+    def connected(self, x, y):
+      return self.__root(x) == self.__root(y)
+
+    def union(self, x, y):
+      i, j = self.__root(x), self.__root(y)
+      self.array[i] = j
+
+    def __root(self, i):
+      while i != self.array[i]:
+        i = self.array[i]
+      return i
+
+class QuickUnionUFTest(unittest.TestCase):
+  
+    def setUp(self):
+        self.obj = QuickUnionUF(10)
+
+    def test_initialize(self):
+        expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(self.obj.array, expected)
+
+    def test_connected(self):
+        self.assertFalse(self.obj.connected(2, 3))
+
+    def test_union(self):
+        self.obj.union(2, 3)
+        self.assertTrue(self.obj.connected(2, 3))
+
+if __name__ == '__main__':
+    unittest.main()
