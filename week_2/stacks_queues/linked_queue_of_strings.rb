1e6ce79f21749d2049a28fe6e7d0ae5b24307d74
Mauro Berlanda <m.berlanda@fit2you.it>
2016-02-25 22:03:49 +0100

    linked queue of string created and tested

---
+++ week_2/stacks_queues/linked_queue_of_strings.rb
@@ -0,0 +1,67 @@
+require 'test/unit'
+require_relative 'node'
+
+class LinkedQueueOfStrings
+
+  attr_reader :first, :last
+
+  def initialize
+    @first = nil
+    @last = nil
+  end
+
+  def is_empty
+    @first == nil
+  end
+
+  def enqueue item
+    oldlast = @last
+    @last = Node.new
+    @last.set_item(item)
+    @last.set_next(nil)
+    if is_empty
+      @first = @last
+    else
+      oldlast.set_next(@last)
+    end
+  end
+
+  def dequeue
+    begin
+      item = @first.item
+      @first = @first.next
+      @last = nil if is_empty
+      return item
+    rescue Exception => e
+      e.message
+    end
+  end
+end
+
+class LinkedQueueOfStringsTest < Test::Unit::TestCase
+  
+  def setup
+    @queue = LinkedQueueOfStrings.new
+  end
+
+
+  def test_is_empty
+    assert_equal(true, @queue.is_empty)
+  end
+
+  def test_empty_dequeue
+    assert_raise(@queue.dequeue)
+  end
+
+  def test_enqueue
+    @queue.enqueue('a')
+    assert_equal(false, @queue.is_empty)
+    assert_equal('a', @queue.first.item)
+  end
+
+  def test_dequeue
+    @queue.enqueue('b')
+    assert_equal('b', @queue.dequeue)
+  end
+
+end
