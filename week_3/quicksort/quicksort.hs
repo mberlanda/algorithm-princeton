5cb2553dbb8aadbfebbf97ee27fdf9a80034b142
Mauro Berlanda <m.berlanda@fit2you.it>
2016-07-22 17:15:22 +0200

    quicksort | haskell second implementation

--- week_3/quicksort/quicksort.hs
+++ week_3/quicksort/quicksort.hs
@@ -1,6 +1,14 @@
+import Data.List(partition)
+
 -- first implementation
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort(x:xs) = smSorted ++ [x] ++ bgSorted
   where smSorted  = quicksort [y | y <- xs, y < x]
         bgSorted = quicksort [y | y <- xs, y >= x]
+
+-- second implementation using Data.List(partion)
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = let (ys, zs) = partition (< x) xs
+               in qsort ys ++ x : qsort zs
